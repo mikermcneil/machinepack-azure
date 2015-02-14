6b371c4535a4294dd37eb948711e81186c5c4028
sedouard <steven.edouard1@gmail.com>
2015-02-13 17:49:57 -0800

    intial

---
+++ machines/create-website.js
@@ -0,0 +1,37 @@
+module.exports = {
+  friendlyName: 'Create Website',
+  description: 'Creates an Azure Website Instance',
+  extendedDescription: '',
+  inputs: {},
+  defaultExit: 'success',
+  exits: { error: { description: 'Unexpected error occurred.' },
+    success: { description: 'Done.' } },
+  fn: function (inputs,exits) {
+    var _ = require('lodash');
+    var child_process = require('child_process');
+    var readline = require('readline');
+    var cliPath = './node_modules/azure-cli/bin/azure';
+    var uuid = require('uuid');
+      var defaults, command;
+
+      defaults = {
+          name: uuid.v4(),
+          location: 'West US',
+          hostname: null
+      };
+
+      _.defaults(inputs, defaults);
+
+      command = 'azure site create --location "' + inputs.location + '" "' + inputs.name + '"';
+
+      child_process.exec(command, function (err, stdout) {
+
+          if (err) {
+              return exists.error(err);
+          }
+
+          return exists.success(stdout);
+      });
+    },
+
+};
